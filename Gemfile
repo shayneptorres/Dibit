d4936baeea42d051291192576416af6cdd7b0e8a
Shayne Torres <shayneptorres@gmail.com>
2016-01-30 02:29:47 -0800

    set of amazon s3

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem "cancan"
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 gem 'friendly_id'
+gem 'fog', '1.36.0'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -9,7 +9,11 @@ class PictureUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [500,500]
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end 
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
